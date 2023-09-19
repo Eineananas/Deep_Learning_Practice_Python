921a5aeb06b632cc9ea0728e3550f7bb1ba2927e
Eineananas <133489015+Eineananas@users.noreply.github.com>
2023-09-19 16:50:34 +0800

    Update CNN_Classification.py

--- CNN/CNN_Classification.py
+++ CNN/CNN_Classification.py
@@ -6,99 +6,94 @@ import torch.optim as optim
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
-# 定义数据转换
+
+# Define data transformations
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-# 下载训练集
+
+# Download the training dataset
 trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 train_loader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
-# 下载测试集
+
+# Download the test dataset
 testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 test_loader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
-# 定义超参数
 
-input_size = 28  #图像的总尺寸28*28
-num_classes = 10  #标签的种类数
-num_epochs = 3  #训练的总循环周期
-batch_size = 64  #一个撮（批次）的大小，64张图片
+# Define hyperparameters
+input_size = 28
+num_classes = 10
+num_epochs = 3
+batch_size = 64
 
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-# 提取整个训练集
+# Extract the entire training set
 x_train = []
-y_train=[]
+y_train = []
 for images, labels in train_loader:
     x_train.append(images)
     y_train.append(labels)
 
-# 提取整个测试集
+# Extract the entire test set
 x_valid = []
 y_valid = []
 for images, labels in test_loader:
     x_valid.append(images)
     y_valid.append(labels)
 
-
 x_train = torch.cat(x_train, dim=0)
 y_train = torch.cat(y_train, dim=0)
 x_valid = torch.cat(x_valid, dim=0)
 y_valid = torch.cat(y_valid, dim=0)
 
-
 x_train, x_train.shape, y_train.min(), y_train.max()
-#print(x_train, y_train)
-print(x_train.shape)
-print(y_train.min(), y_train.max())
 
-#一下是一个很经典的定义CNN网络
+# Define a classic CNN network
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Sequential(         # 输入大小 (1, 28, 28)
+        self.conv1 = nn.Sequential(
             nn.Conv2d(
-                in_channels=1,              # 灰度图
-                out_channels=16,            # 要得到几多少个特征图
-                kernel_size=5,              # 卷积核大小
-                stride=1,                   # 步长
-                padding=2,                  # 如果希望卷积后大小跟原来一样，需要设置padding=(kernel_size-1)/2 if stride=1
-            ),                              # 输出的特征图为 (16, 28, 28)
-            nn.ReLU(),                      # relu层
-            nn.MaxPool2d(kernel_size=2),    # 进行池化操作（2x2 区域）, 28→14，输出结果为： (16, 14, 14)
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
         )
-        self.conv2 = nn.Sequential(         # 下一个套餐的输入 (16, 14, 14)
-            nn.Conv2d(16, 32, 5, 1, 2),     # 输出 (32, 14, 14)
-            nn.ReLU(),                      # relu层
-            nn.MaxPool2d(2),                # 进行池化操作（2x2 区域）, 14→7，输出 (32, 7, 7)
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
         )
-        self.out = nn.Linear(32 * 7 * 7, 10)   # 之前的（32，7，7）拉长之后变成（32*7*7，1），最后输出要是10
+        self.out = nn.Linear(32 * 7 * 7, 10)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
-        x = x.view(x.size(0), -1)           # flatten操作，结果为：(batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)
         output = self.out(x)
         return output
 
+# Function to calculate accuracy
 def accuracy(predictions, labels):
     pred = torch.max(predictions.data, 1)[1]
     rights = pred.eq(labels.data.view_as(pred)).sum()
     return rights, len(labels)
 
-
-# 实例化
+# Instantiate the network
 net = CNN()
-# 损失函数
-criterion = nn.CrossEntropyLoss()
-# 优化器
-optimizer = optim.Adam(net.parameters(), lr=0.001)  # 定义优化器，普通的随机梯度下降算法
 
-# 开始训练循环
+# Loss function
+criterion = nn.CrossEntropyLoss()
+
+# Optimizer
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+# Start training loop
 for epoch in range(num_epochs):
-    # 当前epoch的结果保存下来
     train_rights = []
 
-    for batch_idx, (data, target) in enumerate(train_loader):  # 针对容器中的每一个批进行循环
+    for batch_idx, (data, target) in enumerate(train_loader):
         net.train()
         output = net(data)
         loss = criterion(output, target)
@@ -109,7 +104,6 @@ for epoch in range(num_epochs):
         train_rights.append(right)
 
         if batch_idx % 100 == 0:
-
             net.eval()
             val_rights = []
 
@@ -118,13 +112,12 @@ for epoch in range(num_epochs):
                 right = accuracy(output, target)
                 val_rights.append(right)
 
-            # 准确率计算
             train_r = (sum([tup[0] for tup in train_rights]), sum([tup[1] for tup in train_rights]))
             val_r = (sum([tup[0] for tup in val_rights]), sum([tup[1] for tup in val_rights]))
 
-            print('当前epoch: {} [{}/{} ({:.0f}%)]\t损失: {:.6f}\t训练集准确率: {:.2f}%\t测试集正确率: {:.2f}%'.format(
+            print('Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tTrain Accuracy: {:.2f}%\tTest Accuracy: {:.2f}%'.format(
                 epoch, batch_idx * batch_size, len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader),
+                100. * batch_idx / len(train_loader),
                 loss.data,
-                       100. * train_r[0].numpy() / train_r[1],
-                       100. * val_r[0].numpy() / val_r[1]))
+                100. * train_r[0].numpy() / train_r[1],
+                100. * val_r[0].numpy() / val_r[1]))
