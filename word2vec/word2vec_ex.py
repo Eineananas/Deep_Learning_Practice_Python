51b4bf5ce5ad7502a5ad26b3f67487dac2dea64c
Eineananas <133489015+Eineananas@users.noreply.github.com>
2023-10-01 16:52:42 +0800

    Update word2vec_ex.py

--- word2vec/word2vec_ex.py
+++ word2vec/word2vec_ex.py
@@ -1,20 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# 参考资料：<br>
-# 白话word2vec：https://zhuanlan.zhihu.com/p/81032021 <br>
-# 什么是词向量？https://blog.csdn.net/mawenqi0729/article/details/80698350 <br>
-# 官方参数解读：https://tedboy.github.io/nlps/generated/generated/gensim.models.Word2Vec.html#gensim.models.Word2Vec <br>
-# 为什么PCA：https://zhuanlan.zhihu.com/p/37810506 <br>
-# https://blog.csdn.net/HLBoy_happy/article/details/77146012 <br>
-# 参数解读博客版：https://blog.csdn.net/xiaoQL520/article/details/102509477 <br>
-# 负采样：https://zhuanlan.zhihu.com/p/144146838 <br>
-# 更多资料：https://zhuanlan.zhihu.com/p/26306795 <br>
-# https://mp.weixin.qq.com/s/j8JPMZSPoVT_hQswX5QVxA
-
-# In[64]:
-
-
+# Word Vectors: 50-300 dimensions, two strategies
+# CBOW Model: Predicts center word from surrounding words
+# Skip-gram Model: Predicts surrounding words from center word (more common)
+# Negative Sampling: Introduces some non-existing combinations, marked as 0
+# Window Size: If window size is 2, it considers two words on either side as context words
+# GloVe captures global corpus statistics, combining local context windows like word2vec and global matrix factorization like LSA (a bit like LDA)
+# LSA isn't good at word analogies (e.g., 'man' is to 'woman' as 'king' is to 'queen'), while word2vec maps words to vector spaces and performs analogies
+# LSA captures global corpus statistics
+# Co-occurrence matrix: Symmetric matrix, representing the probability of two words occurring together and the distance between them when they co-occur
+# Elements of co-occurrence matrix calculated as co-occurrence count * weight decay function
 import jieba
 import re
 import numpy as np
@@ -24,113 +17,66 @@ from gensim.models import Word2Vec
 import matplotlib.pyplot as plt
 import matplotlib
 
-
-# ## 分词
-
-# In[65]:
-
-
-f = open("sanguo.txt", 'r',encoding='utf-8') #读入文本
+filePath = r'D:/PyCharm2022/pythonProject2/venv/data/sanguo.txt'
+f = open(filePath, 'r', encoding='utf-8') # Read text
 lines = []
-for line in f: #分别对每段分词
-    temp = jieba.lcut(line)  #结巴分词 精确模式
+for line in f: # Tokenize each line
+    temp = jieba.lcut(line)  # Jieba word segmentation in precise mode
     words = []
     for i in temp:
-        #过滤掉所有的标点符号
+        # Filter out all punctuation marks
         i = re.sub("[\s+\.\!\/_,$%^*(+\"\'””《》]+|[+——！，。？、~@#￥%……&*（）：；‘]+", "", i)
         if len(i) > 0:
             words.append(i)
     if len(words) > 0:
         lines.append(words)
-print(lines[0:5])#预览前5行分词结果
+print(lines[0:5])  # Preview the tokenized results of the first 5 lines
 
+# Call Word2Vec for training
+# Parameters: size - word vector dimension, window - context window width, min_count - minimum word frequency for consideration
+model = Word2Vec(lines, vector_size=20, window=2, min_count=3, epochs=7, negative=10, sg=1)
+print("Word Vector for '孔明':\n", model.wv.get_vector('孔明'))
+print("\nTop 20 words most similar to '孔明':")
+print(model.wv.most_similar('孙权', topn=20))  # Top 20 words most similar to '孔明'
 
-# ## 模型训练 
-
-# In[158]:
-
-
-# 调用Word2Vec训练
-# 参数：size: 词向量维度；window: 上下文的宽度，min_count为考虑计算的单词的最低词频阈值
-model = Word2Vec(lines,vector_size = 20, window = 2 , min_count = 3, epochs=7, negative=10,sg=1)
-print("孔明的词向量：\n",model.wv.get_vector('孔明'))
-print("\n和孔明相关性最高的前20个词语：")
-model.wv.most_similar('孔明', topn = 20)# 与孔明最相关的前20个词语
-
-
-# ## 可视化 
-
-# In[159]:
-
-
-# 将词向量投影到二维空间
+# Visualization
+# Project word vectors into 2D space
 rawWordVec = []
 word2ind = {}
-for i, w in enumerate(model.wv.index_to_key): #index_to_key 序号,词语
-    rawWordVec.append(model.wv[w]) #词向量
-    word2ind[w] = i #{词语:序号}
+for i, w in enumerate(model.wv.index_to_key):  # index_to_key: index, word
+    rawWordVec.append(model.wv[w])  # Word vector
+    word2ind[w] = i  # {word: index}
 rawWordVec = np.array(rawWordVec)
-X_reduced = PCA(n_components=2).fit_transform(rawWordVec) 
+X_reduced = PCA(n_components=2).fit_transform(rawWordVec)
 
-
-# In[160]:
-
-
-rawWordVec #降维之前20维
-
-
-# In[161]:
-
-
-X_reduced #降维之后2维
-
-
-# In[162]:
-
-
-# 绘制星空图
-# 绘制所有单词向量的二维空间投影
-fig = plt.figure(figsize = (15, 10))
+# Plotting
+fig = plt.figure(figsize=(15, 10))
 ax = fig.gca()
 ax.set_facecolor('white')
-ax.plot(X_reduced[:, 0], X_reduced[:, 1], '.', markersize = 1, alpha = 0.3, color = 'black')
+ax.plot(X_reduced[:, 0], X_reduced[:, 1], '.', markersize=1, alpha=0.3, color='black')
 
+# Plot star chart
+# Plot the 2D space projection of all word vectors
+fig = plt.figure(figsize=(15, 10))
+ax = fig.gca()
+ax.set_facecolor('white')
+ax.plot(X_reduced[:, 0], X_reduced[:, 1], '.', markersize=1, alpha=0.3, color='black')
 
-# 绘制几个特殊单词的向量
-words = ['孙权', '刘备', '曹操', '周瑜', '诸葛亮', '司马懿','汉献帝']
-
-# 设置中文字体 否则乱码
+# Plot vectors for specific words
+words = ['孙权', '刘备', '曹操', '周瑜', '诸葛亮', '司马懿', '汉献帝']
 zhfont1 = matplotlib.font_manager.FontProperties(fname='./华文仿宋.ttf', size=16)
+# Set Chinese font, otherwise it might display as gibberish
 for w in words:
     if w in word2ind:
         ind = word2ind[w]
         xy = X_reduced[ind]
-        plt.plot(xy[0], xy[1], '.', alpha =1, color = 'orange',markersize=10)
-        plt.text(xy[0], xy[1], w, fontproperties = zhfont1, alpha = 1, color = 'red')
+        plt.plot(xy[0], xy[1], '.', alpha=1, color='orange', markersize=10)
+        plt.text(xy[0], xy[1], w, fontproperties=zhfont1, alpha=1, color='red')
+plt.show()
 
-
-# ## 类比关系实验
-
-# In[163]:
-
-
-# 玄德－孔明＝？－曹操
+# Perform some special word vector operations
 words = model.wv.most_similar(positive=['玄德', '曹操'], negative=['孔明'])
-words
-
-
-# In[164]:
-
-
-# 曹操－魏＝？－蜀
+print("a", words)
 words = model.wv.most_similar(positive=['曹操', '蜀'], negative=['魏'])
-words
+print("b", words)
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
